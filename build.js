b494f731970457f56bd41800c9a54131fbd00f4c
Jannis R <mail@jannisr.de>
2018-02-12 19:55:48 +0100

    build CSV and JSON files

---
+++ build.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const path = require('path')
+const toCsv = require('csv-write-stream')
+const pump = require('pump')
+const fs = require('fs')
+
+const full = require('./full')
+
+const showError = (err) => {
+	if (!err) return null
+	console.error(err)
+	process.exit(1)
+}
+
+for (let set of Object.keys(full)) {
+	// write CSV
+	const sink = toCsv()
+	pump(
+		sink,
+		fs.createWriteStream(path.join(__dirname, 'csv', set + '.csv')),
+		showError
+	)
+	if (Array.isArray(full[set])) {
+		for (let item of full[set]) sink.write(item)
+	} else sink.write(full[set])
+	sink.end()
+
+	// write JSON
+	const dest = path.join(__dirname, 'json', set + '.json')
+	fs.writeFile(dest, JSON.stringify(full[set]), showError)
+}
