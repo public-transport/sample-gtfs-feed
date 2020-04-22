8509eb74367f8a9d6dcbbfdb41a198588021b555
Jannis R <mail@jannisr.de>
2020-04-22 16:23:25 +0200

    stop_times: fix arrival/departure times :bug:, 0.3.4

--- stop_times.js
+++ stop_times.js
@@ -18,34 +18,34 @@ const applyToTrips = (trips, seq, arr, dep, stop, more = {}) => {
 }
 
 const forADowntown = [].concat(
-	applyToTrips(aDowntown, 3, '15:23', '15:24', airport.station),
-	applyToTrips(aDowntown, 4, '15:30', '15:31', museum.station),
-	applyToTrips(aDowntown, 5, '15:35', '15:36', center.station)
+	applyToTrips(aDowntown, 3, '15:23:00', '15:24:00', airport.station),
+	applyToTrips(aDowntown, 4, '15:30:00', '15:31:00', museum.station),
+	applyToTrips(aDowntown, 5, '15:35:00', '15:36:00', center.station)
 )
 
 const forAOutbound = [].concat(
-	applyToTrips(aOutbound, 2, '17:13', '17:14', center.station),
-	applyToTrips(aOutbound, 3, '17:20', '17:21', museum.station),
-	applyToTrips(aOutbound, 4, '17:25', '17:26', airport.station)
+	applyToTrips(aOutbound, 2, '17:13:00', '17:14:00', center.station),
+	applyToTrips(aOutbound, 3, '17:20:00', '17:21:00', museum.station),
+	applyToTrips(aOutbound, 4, '17:25:00', '17:26:00', airport.station)
 )
 
 const forBDowntown = [].concat(
-	applyToTrips(bDowntown, 1, '13:13', '13:14', airport.station),
-	applyToTrips([bDowntown[0]], 3, '13:20', '13:22', lake.station),
-	applyToTrips(bDowntown.slice(1), 3, '13:22', '13:24', lake.station),
-	applyToTrips(bDowntown, 5, '13:30', '13:31', center.station)
+	applyToTrips(bDowntown, 1, '13:13:00', '13:14:00', airport.station),
+	applyToTrips([bDowntown[0]], 3, '13:20:00', '13:22:00', lake.station),
+	applyToTrips(bDowntown.slice(1), 3, '13:22:00', '13:24:00', lake.station),
+	applyToTrips(bDowntown, 5, '13:30:00', '13:31:00', center.station)
 )
 
 const forBOutbound = [].concat(
-	applyToTrips(bOutbound, 11, '18:13', '18:14', center.station),
-	applyToTrips([bOutbound[0]], 13, '18:20', '18:22', lake.station),
-	applyToTrips(bOutbound.slice(1), 13, '18:22', '18:24', lake.station),
-	applyToTrips(bOutbound, 15, '18:30', '18:31', airport.station)
+	applyToTrips(bOutbound, 11, '18:13:00', '18:14:00', center.station),
+	applyToTrips([bOutbound[0]], 13, '18:20:00', '18:22:00', lake.station),
+	applyToTrips(bOutbound.slice(1), 13, '18:22:00', '18:24:00', lake.station),
+	applyToTrips(bOutbound, 15, '18:30:00', '18:31:00', airport.station)
 )
 
 const forCDowntown = [].concat(
-	applyToTrips(cDowntown, 0, '15:27', '15:28', airport.station),
-	applyToTrips(cDowntown, 1, '15:33', '15:35', center.station)
+	applyToTrips(cDowntown, 0, '15:27:00', '15:28:00', airport.station),
+	applyToTrips(cDowntown, 1, '15:33:00', '15:35:00', center.station)
 )
 
 // todo: 2 trips for one schedule
