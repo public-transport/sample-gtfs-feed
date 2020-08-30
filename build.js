17d19ead847b811d8ca44f6a0cc596065e69e035
Jannis R <mail@jannisr.de>
2020-08-30 13:16:42 +0200

    build: refactor a bit

--- build.js
+++ build.js
@@ -14,11 +14,13 @@ const showError = (err) => {
 }
 
 for (let set of Object.keys(full)) {
+	const data = full[set]
+
 	// determine columns
 	let cols = undefined
-	if (Array.isArray(full[set])) {
+	if (Array.isArray(data)) {
 		cols = new Set()
-		for (const item of full[set]) {
+		for (const item of data) {
 			for (const col of Object.keys(item)) cols.add(col)
 		}
 		cols = Array.from(cols.values())
@@ -31,14 +33,14 @@ for (let set of Object.keys(full)) {
 		fs.createWriteStream(path.join(__dirname, 'gtfs', set + '.txt')),
 		showError
 	)
-	if (Array.isArray(full[set])) {
-		for (let item of full[set]) {
+	if (Array.isArray(data)) {
+		for (let item of data) {
 			sink.write(item)
 		}
-	} else sink.write(full[set])
+	} else sink.write(data)
 	sink.end()
 
 	// write JSON
 	const dest = path.join(__dirname, 'json', set + '.json')
-	fs.writeFile(dest, JSON.stringify(full[set]), showError)
+	fs.writeFile(dest, JSON.stringify(data), showError)
 }
