8bee05748f760fcbdbb8e45dd6c531adde197b67
Jannis R <mail@jannisr.de>
2021-03-14 18:44:36 +0100

    make pathways more complex 💥, 0.8.0
    - make airport-entrance -> airport-1-access unidirectional 💥
    - add "reverse" counterpart to airport-entrance -> airport-1-access
    - add airport -> museum tunnel, connecting to *stations*

--- pathways.js
+++ pathways.js
@@ -1,17 +1,19 @@
 'use strict'
 
-const {airport} = require('./stops')
+const {airport, museum: _museum} = require('./stops')
 
 const WALKWAY = '1'
 const STAIRS = '2'
 const ESCALATOR = '4'
 const ELEVATOR = '5'
+const EXIT_GATE = '7'
 
 const entrance = airport.find(({stop_id}) => stop_id === 'airport-entrance')
 const pl1 = airport.find(({stop_id}) => stop_id === 'airport-1')
 const pl1AccessNode = airport.find(({stop_id}) => stop_id === 'airport-1-access')
 const pl2Boarding = airport.find(({stop_id}) => stop_id === 'airport-2-boarding')
 const pl2AccessNode = airport.find(({stop_id}) => stop_id === 'airport-2-access')
+const museum = _museum.find(({stop_id}) => stop_id === 'museum')
 
 let i = 0
 const id = () => i++ + ''
@@ -22,13 +24,21 @@ const airportEntranceToPl1 = [{
 	from_stop_id: entrance.stop_id,
 	to_stop_id: pl1AccessNode.stop_id,
 	pathway_mode: ESCALATOR,
-	is_bidirectional: '1',
+	is_bidirectional: '0',
 }, {
 	pathway_id: id(),
 	from_stop_id: pl1AccessNode.stop_id,
 	to_stop_id: pl1.stop_id,
 	pathway_mode: STAIRS,
 	is_bidirectional: '1',
+}]
+const pl1ToAirportEntrance = [{
+	pathway_id: id(),
+	from_stop_id: pl1AccessNode.stop_id,
+	to_stop_id: entrance.stop_id,
+	pathway_mode: EXIT_GATE,
+	is_bidirectional: '0',
+	signposted_as: 'exit',
 }]
 
 // wheelchair-accessible
@@ -52,11 +62,25 @@ const airportEntranceToPl2Boarding = [{
 	is_bidirectional: '1',
 }]
 
+// unrealistic pathway that connects another *station*
+const airportToMuseumTunnel = [{
+	pathway_id: id(),
+	from_stop_id: entrance.stop_id,
+	to_stop_id: museum.stop_id,
+	pathway_mode: WALKWAY,
+	is_bidirectional: '1',
+	length: 1250,
+}]
+
 const all = [].concat(
 	airportEntranceToPl1,
+	pl1ToAirportEntrance,
 	airportEntranceToPl2Boarding,
+	airportToMuseumTunnel,
 )
 module.exports = Object.assign(all, {
 	airportEntranceToPl1,
+	pl1ToAirportEntrance,
 	airportEntranceToPl2Boarding,
+	airportToMuseumTunnel,
 })
