bb371aa82f3370089bb4d02fb097f95437723cda
Jannis R <mail@jannisr.de>
2021-03-25 15:24:00 +0100

    Node 12+ 💥💚; refactor build script

--- build.js
+++ build.js
@@ -1,10 +1,12 @@
 'use strict'
 
+const {promisify} = require('util')
+const {pipeline} = require('stream')
+const toCsv = require('csv-write-stream')
+const {createWriteStream} = require('fs')
 const sortBy = require('lodash/sortBy')
 const path = require('path')
-const toCsv = require('csv-write-stream')
-const pump = require('pump')
-const fs = require('fs')
+const {writeFile} = require('fs').promises
 
 const full = require('./full')
 
@@ -19,45 +21,40 @@ const orders = Object.assign(Object.create(null), {
 	'frequencies': ['trip_id', 'start_time'],
 })
 
-const showError = (err) => {
-	if (!err) return null
-	console.error(err)
-	process.exit(1)
-}
+const pPipeline = promisify(pipeline)
 
-for (let set of Object.keys(full)) {
-	let data = full[set]
-
-	// determine columns
-	let cols = undefined
-	if (Array.isArray(data)) {
-		cols = new Set()
-		for (const item of data) {
-			for (const col of Object.keys(item)) cols.add(col)
-		}
-		cols = Array.from(cols.values())
-	}
-
-	// sort data
-	if (Array.isArray(data) && (set in orders)) {
-		data = sortBy(data, orders[set])
-	}
-
-	// write GTFS
+const writeCsv = async (dest, cols, rows) => {
 	const sink = toCsv({headers: cols})
-	pump(
-		sink,
-		fs.createWriteStream(path.join(__dirname, 'gtfs', set + '.txt')),
-		showError
-	)
-	if (Array.isArray(data)) {
-		for (let item of data) {
-			sink.write(item)
-		}
-	} else sink.write(data)
+	const task = pPipeline(sink, createWriteStream(dest))
+
+	for (const row of rows) sink.write(row)
 	sink.end()
 
-	// write JSON
-	const dest = path.join(__dirname, 'json', set + '.json')
-	fs.writeFile(dest, JSON.stringify(data), showError)
+	await task
 }
+
+;(async () => {
+	for (const set of Object.keys(full)) {
+		const data = full[set]
+
+		const rows = Array.isArray(data)
+			? (set in orders ? sortBy(data, orders[set]) : data)
+			: [data]
+		const cols = Array.from(new Set(rows.flatMap(row => Object.keys(row))))
+
+		// write GTFS
+		await writeCsv(
+			path.join(__dirname, 'gtfs', set + '.txt'),
+			cols,
+			rows,
+		)
+
+		// write JSON
+		const dest = path.join(__dirname, 'json', set + '.json')
+		await writeFile(dest, JSON.stringify(data))
+	}
+})()
+.catch((err) => {
+	console.error(err)
+	process.exit(1)
+})
